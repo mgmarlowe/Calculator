e375d8ee1a20e2c34bbbcff550d56864f6be1fe8
Melyssa <m.g.mayhem@gmail.com>
2021-07-29 16:53:00 -0400

    Add backspace and all clear functionality

--- script.js
+++ script.js
@@ -1,15 +1,38 @@
 const nums = document.querySelectorAll(".nums");
 nums.forEach((num) => num.addEventListener('click', getNum));
 
-let operands = [];
-let a = operands[0];
-let b = operands[1];
+let displayValue = 0;
+
+document.getElementById("AC").addEventListener("click", function () {
+    displayValue = 0;
+    updateDisplay();
+})
+
+document.getElementById("C").addEventListener("click", function() {
+    displayValue = displayValue
+        .toString()
+        .slice(0, -1);
+    
+    updateDisplay();
+
+})
 
 function getNum(e) {
     let num = e.target.value;
-    operands.push(num);
-    document.getElementById("answer-screen").textContent = operands.join("");
+    if (displayValue === 0 || displayValue === "0") {
+        displayValue = num;
+    }
+    else{
+        displayValue += num;
+    }
+    updateDisplay();
 }
+
+function updateDisplay() {
+    document.getElementById("answer-screen").textContent = displayValue;
+}
+
+updateDisplay();
 
 function add (a ,b) {
     console.log(a);
