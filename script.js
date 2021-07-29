d5b754711dc8513396fd1ef9b85a67375bcbd21c
Melyssa <m.g.mayhem@gmail.com>
2021-07-29 16:00:49 -0400

    Add limited functionality

--- script.js
+++ script.js
@@ -1,4 +1,15 @@
-let nums = [];
+const nums = document.querySelectorAll(".nums");
+nums.forEach((num) => num.addEventListener('click', getNum));
+
+let operands = [];
+let a = operands[0];
+let b = operands[1];
+
+function getNum(e) {
+    let num = e.target.value;
+    operands.push(num);
+    document.getElementById("answer-screen").textContent = operands.join("");
+}
 
 function add (a ,b) {
     console.log(a);
